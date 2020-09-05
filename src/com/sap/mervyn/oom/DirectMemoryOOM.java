65988afdc58ec9b15142f7d064e2806cfb6db693
I347764 <mervyn.chen@sap.com>
2020-09-05 18:29:04 +0800

    gc commit for jvm study

---
+++ src/com/sap/mervyn/oom/DirectMemoryOOM.java
@@ -0,0 +1,23 @@
+package com.sap.mervyn.oom;
+
+import java.lang.reflect.Field;
+
+import sun.misc.Unsafe;
+
+/*
+ * -Xmx20M -XX:MaxDirectMemorySize=10M
+ */
+public class DirectMemoryOOM {
+	private static final int _1MB = 1024 * 1024;
+	
+	public static void main(String[] args) throws IllegalArgumentException, IllegalAccessException {
+		Field unsafeField = Unsafe.class.getDeclaredFields()[0];
+		unsafeField.setAccessible(true);
+		
+		Unsafe unsafe = (Unsafe) unsafeField.get(null);
+		
+		while (true) {
+			unsafe.allocateMemory(_1MB);
+		}
+	}
+}
